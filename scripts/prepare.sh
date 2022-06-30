8e852d234d883c92ff67fd33b1628c8ef83d15c0
VH🔥 <vherman@vf-soft.com>
2022-06-30 14:00:58 +0300

    fix: dOCKER_REPOSITORY_NAME

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,3 +1,3 @@
-echo "{\"sha\":\"$GITHUB_SHA\", \"semver\":\"$2\"}" > semver.json
+echo "{\"sha\":\"$GITHUB_SHA\", \"semver\":\"$DOCKER_REPOSITORY_NAME:$2\"}" > semver.json
 cat semver.json
 docker build -t "$1" .
